3bbeadae32e19910bdd0ab8fd8243571aa94cdc9
Daniel Martin <dlmfxq@umsystem.edu>
2022-12-09 21:44:03 -0600

    modify modifyItem()

--- main.py
+++ main.py
@@ -362,6 +362,7 @@ def delete_item():
 #modifies item in DBMS
 def modify_item():
     validInput = False
+    DEPS = ("Meat","Bakery","Seafood","Deli","Grocery")
     IDs = []
     #welcome message
     print("---MODIFY an Item from Inventory---\n")
@@ -403,40 +404,40 @@ def modify_item():
             print("---Please insert a valid number---")
     validInput = False
     clearScreen()
-    print("---MODIFY an Item from Inventory---\n")
+    
     if prompt == 1:
         while not validInput:
+            print("---MODIFY an Item from Inventory---\n")
             newName = input("Enter the new name for the item: ")
-            prompt = input(f"Item's name will be changed to {newName}. Is this correct? y/n")
+            prompt = input(f"Item's name will be changed to {newName}. Is this correct? y/n ")
             clearScreen()
             if prompt.lower()=='y' and not prompt.isspace():
-                validInput == True
+                validInput = True
                 cursor.execute(f"UPDATE ITEM set Item_Name='{newName}' where Item_ID = {item_id}")
-                input("Item modified. Press ENTER to continue...")
                 clearScreen()
 
                 
     elif prompt == 2:
         while not validInput:
+            print("---MODIFY an Item from Inventory---\n")
             newName = input("Enter the new brand for the item: ")
-            prompt = input(f"Item's brand will be changed to {newName}. Is this correct? y/n")
+            prompt = input(f"Item's brand will be changed to {newName}. Is this correct? y/n ")
             clearScreen()
             if prompt.lower()=='y' and not prompt.isspace():
-                validInput == True
+                validInput = True
                 cursor.execute(f"UPDATE ITEM set Brand='{newName}' where Item_ID = {item_id};")
-                input("Item modified. Press ENTER to continue...")
                 clearScreen()
  
     elif prompt == 3:
         while not validInput:
             try:
+                print("---MODIFY an Item from Inventory---\n")
                 newAisle = int(input("Enter a new aisle number for the item: "))
-                prompt = input(f"Item's location will be changed to aisle {newAisle}. Is this correct? y/n")
+                prompt = input(f"Item's location will be changed to aisle {newAisle}. Is this correct? y/n ")
                 clearScreen()
                 if prompt.lower()=='y' and not prompt.isspace():
-                    validInput == True
+                    validInput = True
                     cursor.execute(f"UPDATE ITEM set Location='Aisle {newAisle}' where Item_ID = {item_id};")
-                    input("Item modified. Press ENTER to continue...")
                     clearScreen()
 
             except ValueError:
@@ -445,14 +446,17 @@ def modify_item():
     elif prompt == 4:
         while not validInput:
             try:
+                    
+                print("---MODIFY an Item from Inventory---\n")
                 newPrice = float(input("Enter a new price for the item: $ "))
-                prompt = input(f"Item's location will be changed to aisle {newPrice}. Is this correct? y/n")
+                print(f"The calculated profit for this item has been updated to ${f'{round(newPrice%4*.95,2):.2f}'}")
+                prompt = input(f"Item's price will be changed to ${f'{newPrice:.2f}'} Is this correct? y/n ")
+                
                 clearScreen()
                 if prompt.lower()=='y' and not prompt.isspace():
-                    validInput == True
+                    validInput = True
                     cursor.execute(f"UPDATE ITEM set Price={newPrice} where Item_ID = {item_id};")
-                    cursor.execute(f"UPDATE ITEM set Prof_Per={newPrice%4*.99+1} where Item_ID = {item_id};")
-                    input("Item modified. Press ENTER to continue...")
+                    cursor.execute(f"UPDATE ITEM set Prof_Per={round(newPrice%4*.95,2)} where Item_ID = {item_id};")
                     clearScreen()
                     
             except ValueError:
@@ -461,20 +465,22 @@ def modify_item():
     elif prompt == 5:
         while not validInput:
             try:
+                print("---MODIFY an Item from Inventory---\n")
                 newStock = int(input("Enter a new stock amount for the item: "))
-                prompt = input(f"Item's stock amount will be changed to {newStock}. Is this correct? y/n")
+                prompt = input(f"Item's stock amount will be changed to {newStock}. Is this correct? y/n ")
                 clearScreen()
                 if prompt.lower()=='y' and not prompt.isspace():
-                    validInput == True
+                    validInput = True
                     cursor.execute(f"UPDATE ITEM set Stock_Amount={newStock} where Item_ID = {item_id};")
-                    input("Item modified. Press ENTER to continue...")
                     clearScreen()
                     
             except ValueError:
                 print("Please enter a valid number")
                 clearScreen()
     elif prompt == 6:
+        
         while not validInput:
+            print("---MODIFY an Item from Inventory---\n")
             try:
                 print(f"Which department would you like to change the item to?\n")
                 print(" 1 Meat")
@@ -484,18 +490,19 @@ def modify_item():
                 print(" 5 Grocery")
                 item_Dep_num = int(input("\nSelect Department based on number (1-5): "))
                 clearScreen()
-                prompt = input(f"Item's department will be changed to {item_Dep_num}. Is this correct? y/n")
+                
+                prompt = input(f"Item's department will be changed to {DEPS[item_Dep_num-1]}. Is this correct? y/n ")
                 clearScreen()
                 if prompt.lower()=='y' and not prompt.isspace() and item_Dep_num in (1,2,3,4,5):
-                    validInput == True
+                    print("Im here")
+                    validInput = True
                     cursor.execute(f"UPDATE ITEM set Dep_Num={item_Dep_num} where Item_ID = {item_id};")
-                    input("Item modified. Press ENTER to continue...")
                     clearScreen()
             except ValueError:
                 print("---Please enter a valid NUMBER")
-    
+        
     clearScreen()
-    print("---MODIFY an Item from Inventory---\n")
+    print("---Item Modified---\n")
     view_items(item_id=item_id)
     input("\nPress ENTER to continue...")
     return
@@ -761,7 +768,6 @@ def main():
 
     # [('DEPARTMENT',), ('EMERGENCY_CONTACT',), ('EMPLOYEE',), ('ITEM',), ('PRODUCTS',), ('SOLD',), ('TRANSACTION',), ('VENDOR',)]
 
-    get_transaction_report()
     
     
 if __name__ == "__main__":
